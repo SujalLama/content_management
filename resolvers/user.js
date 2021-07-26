0dae252ff295a24d9de5f0e05a07b298b2a03d52
Sujal Lama <lsujal41@gmail.com>
2021-07-26 22:08:10 +0545

    initial coniguration: PENG stack

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,115 +1,115 @@
 // const bcrypt = require('bcryptjs');
 
-// module.exports = {
-//     Query: {
-//         allUsers: async (parent, args, {db, user}) => {
-//             try {
-//                 if(!user) return {
-//                     message: "You are not allowed.",
-//                     success: false,
-//                     users: []
-//                 }
+module.exports = {
+    Query: {
+        allUsers: async (parent, args, {db, user}) => {
+            // try {
+            //     if(!user) return {
+            //         message: "You are not allowed.",
+            //         success: false,
+            //         users: []
+            //     }
 
-//             const users = await db.User.findAll();
+            // const users = await db.User.findAll();
 
-//             return {
-//                 users,
-//                 message: "All users are fetched",
-//                 success: true
-//             }
-//             } catch (error) {
-//                 return {
-//                     message: error.message,
-//                     success: false
-//                     }
-//             }
-//         },
-//         getUser: async (parent, {id}, {db, user}) => {
-//              try {
-//                 if(!user) return {
-//                     message: "You are not allowed.",
-//                     success: false,
-//                     user: {}
-//                 }
+            // return {
+            //     users,
+            //     message: "All users are fetched",
+            //     success: true
+            // }
+            // } catch (error) {
+            //     return {
+            //         message: error.message,
+            //         success: false
+            //         }
+            // }
+        },
+        getUser: async (parent, {id}, {db, user}) => {
+            //  try {
+            //     if(!user) return {
+            //         message: "You are not allowed.",
+            //         success: false,
+            //         user: {}
+            //     }
 
-//             const fetchedUser = await db.User.findOne({where: {id}});
+            // const fetchedUser = await db.User.findOne({where: {id}});
 
-//             if(!fetchedUser) return {
-//                  message: "User doesn't exists.",
-//                     success: false,
-//                     user: {}
-//             }
+            // if(!fetchedUser) return {
+            //      message: "User doesn't exists.",
+            //         success: false,
+            //         user: {}
+            // }
 
-//             return {
-//                 user: fetchedUser,
-//                 message: "Single user is fetched",
-//                 success: true
-//             }
-//             } catch (error) {
-//                 return {
-//                     message: error.message,
-//                     success: false
-//                     }
-//             }
-//         }
-//     },
+            // return {
+            //     user: fetchedUser,
+            //     message: "Single user is fetched",
+            //     success: true
+            // }
+            // } catch (error) {
+            //     return {
+            //         message: error.message,
+            //         success: false
+            //         }
+            // }
+        }
+    },
 
-//     Mutation: {
-//         registerUser: async (parent, args, {db}) => {
-//             try {
-//                 const user = await db.User.create(args)
-//                 const token = user.generateToken()
-//                 return {
-//                     message: 'User is created',
-//                     success: true,
-//                     token
-//                 }
+    Mutation: {
+        registerUser: async (parent, args, {db}) => {
+            // try {
+            //     const user = await db.User.create(args)
+            //     const token = user.generateToken()
+            //     return {
+            //         message: 'User is created',
+            //         success: true,
+            //         token
+            //     }
 
-//             } catch (error) {
-//                 return {
-//                     message: error.message,
-//                     success: false,
-//                     token: ""
-//                     }
-//             }
-//         },
-//         loginUser: async (parent, {email, password}, {db}) => {
-//             try {
+            // } catch (error) {
+            //     return {
+            //         message: error.message,
+            //         success: false,
+            //         token: ""
+            //         }
+            // }
+        },
+        loginUser: async (parent, {email, password}, {db}) => {
+            // try {
 
-//                 if((!email || !password)) return {
-//                     message: "Empty password or username",
-//                     success: false,
-//                     token: ""
-//                 }
-//                 const user = await db.User.findOne({where: {email}})
-//                 if(!user) return {
-//                     message: "user donesn't exist.",
-//                     success: false,
-//                     token: ""
-//                 }
+            //     if((!email || !password)) return {
+            //         message: "Empty password or username",
+            //         success: false,
+            //         token: ""
+            //     }
+            //     const user = await db.User.findOne({where: {email}})
+            //     if(!user) return {
+            //         message: "user donesn't exist.",
+            //         success: false,
+            //         token: ""
+            //     }
 
-//                 if(!bcrypt.compareSync(password, user.password)) {
-//                     return {
-//                         message: "Invalid credentials",
-//                         success: false,
-//                         token: ""
-//                     }
-//                 }
+            //     if(!bcrypt.compareSync(password, user.password)) {
+            //         return {
+            //             message: "Invalid credentials",
+            //             success: false,
+            //             token: ""
+            //         }
+            //     }
 
-//                 const token = user.generateToken()
-//                     return {
-//                         message: "successfully signed in",
-//                         success: true,
-//                         token 
-//                     }
+            //     const token = user.generateToken()
+            //         return {
+            //             message: "successfully signed in",
+            //             success: true,
+            //             token 
+            //         }
 
-//             } catch (error) {
-//                 return {
-//                     message: error.message,
-//                     success: false,
-//                     token: ""
-//                     }
-//             }
-//         }
-//     }
-// }
+            // } catch (error) {
+            //     return {
+            //         message: error.message,
+            //         success: false,
+            //         token: ""
+            //         }
+            // }
+        }
+    }
+}
